0928a8132b3c0d2f4f247115f955057e6c907587
mengju06 <100680536+mengju06@users.noreply.github.com>
2022-03-01 18:04:30 +0800

    Add files via upload

---
+++ ClockAngle.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    private float hour;
+    private float minute;
+    private float angle = 0;
+    static int[] num = new int[2];
+
+    public void set(float hour, float minute){
+        this.hour = (float) ((hour*60 + minute)*0.5);
+        this.minute = minute * 6;
+        angle = (this.minute - this.hour)%360;
+        if(angle < 0)
+            angle = 360 + angle;
+        if(angle > 180)
+            angle = 360 - angle;
+        if(angle == 0)
+            System.out.println("0.0" + "度");
+        else
+            System.out.println("angle :" + " " + angle + "度");
+    }
+
+    public static void split(String input){
+        String[] arr = input.split(":");
+
+        for (int i = 0; i < arr.length; i++) {
+            num[i] = Integer.parseInt(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        App time = new App();
+        System.out.println("INPUT:");
+        Scanner scanInput = new Scanner(System.in);
+        String input = scanInput.nextLine();
+        split(input);
+        time.set(num[0], num[1]);
+    }
+}
